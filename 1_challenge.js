90e36fba3966cac160ed570ab00075c485a06112
delayedaa <delayedaa@gmail.com>
2020-10-27 12:20:33 -0500

    [1_challenge.js] Add solutions to both challenges

--- 1_challenge.js
+++ 1_challenge.js
@@ -3,7 +3,9 @@ Make a function createNewElement(tagName, text) that takes in 2
 parameters and returns a new element with the correct text
 ************************************************************** */
 function createNewElement(tagName, text) {
-    
+  let newElement = document.createElement(tagName);
+  newElement.textContent = text;
+  return newElement;
 }
 
 console.log(createNewElement('p', 'I am a software engineer'));
@@ -18,13 +20,18 @@ Make a function giveMeKeys(array) that takes in an array of objects
 and returns a new array with all of the keys from each object
 ************************************************************** */
 const instuctors = [
-    { rome: 'Rome', pete: 'Pete'},
-    { mike: 'Mike', dex: 'Dexter'},
-    { ga: 'General Assembly', cohort: 'SEI1019' }
+  { rome: 'Rome', pete: 'Pete' },
+  { mike: 'Mike', dex: 'Dexter' },
+  { ga: 'General Assembly', cohort: 'SEI1019' }
 ]
 
 const giveMeKeys = (array) => {
-
+  let newArray = [];
+  array.forEach(instructor => {
+    let keys = Object.keys(instructor);
+    newArray = newArray.concat(keys);
+  });
+  return newArray;
 }
 
 console.log(giveMeKeys(instuctors));
